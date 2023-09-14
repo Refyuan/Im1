a598fbbba2cbf28c30c865063cd661b12286d84a
hys <3154275278@qq.com>
2023-09-14 21:12:54 +0800

    init

---
+++ src/main/java/com/hys/im1/Im1Application.java
@@ -0,0 +1,13 @@
+package com.hys.im1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Im1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Im1Application.class, args);
+    }
+
+}
